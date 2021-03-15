68da96a09513d97fa971f575d06449f8f77fcde7
nicogulo <nicogulobelas@gmail.com>
2021-03-15 13:38:06 +0700

    added icon text and header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+
+
 import "assets/scss/style.scss";
+import LandingPage from "pages/LandingPage";
 
 function App() {
   return (
     <div className="App">
-      Hello world
+     <Router>
+      <Route path="/" component={LandingPage}></Route>
+     </Router>
     </div>
   );
 }
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import Button from 'elements/Button'
+import Header from 'parts/Header'
 export default class LandingPage extends Component {
   render(){
     return(
-      <div>
-      </div>
-    )
+    <>
+    <Header {...this.props}></Header>
+    </>
+    );
   }
 }
