d5e19bf568b3b4eac9f0aced2f2a6e5c1786afbd
nicogulo <nicogulobelas@gmail.com>
2021-03-15 14:40:20 +0700

    fix header

--- src/parts/Header.js
+++ src/parts/Header.js
@@ -24,7 +24,7 @@ export default function Header(props) {
             </li>
             <li className={`nav-item${getNavLinkClass("/brows-by")}`}>
               <Button className="nav-link" type="link" href="/brows-by">
-                Brows By
+                Browse By
               </Button>
             </li>
             <li className={`nav-item${getNavLinkClass("/stories")}`}>
