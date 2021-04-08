a7cfa689d597cd92072595d8308d1c5ba5d9faec
nicogulo <nicogulobelas@gmail.com>
2021-04-08 11:35:38 +0700

    added container grid

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -4,29 +4,23 @@ export default function MostPicked(props) {
   return (
     <section className="container">
       <h4 className="mb-3">Most Picked</h4>
-      <div className="row">
-        <div className="col-4 ">
-          <div className="card h-100">Card</div>
-        </div>
-        <div className="col-8">
-          <div className="row mb-3">
-            <div className="col-6">
-              <div class="card">Card</div>
-            </div>
-            <div className="col-6">
-              <div class="card">Card</div>
-            </div>
+        <div className="container-grid">
+          <div className="item column-4 row-2">
+            <div className="card">Card 1</div>  
           </div>
-          <div className="row">
-            <div className="col-6">
-              <div class="card">Card</div>
-            </div>
-            <div className="col-6">
-              <div class="card">Card</div>
-            </div>
+          <div className="item column-4 row-1">
+            <div className="card">Card 2</div> 
+          </div>
+          <div className="item column-4 row-1">
+            <div className="card">Card 3</div> 
+          </div>
+          <div className="item column-4 row-1">
+            <div className="card">Card 4</div> 
+          </div>
+          <div className="item column-4 row-1">
+            <div className="card">Card 5</div> 
+          </div>
         </div>
-        </div>
-      </div>
     </section>
   )
 }
