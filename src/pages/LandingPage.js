4cc1247f9e2aa1e3674ee1d6b71b6ccc698787a3
nicogulo <nicogulobelas@gmail.com>
2021-04-08 14:55:04 +0700

    fix error map and markup  mostpicked complete

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,15 +4,15 @@ import Header from 'parts/Header'
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 
-import landingPage from 'json/landingPage';
-
+import landingPage from 'json/landingPage.json';
 export default class LandingPage extends Component {
   render(){
+    console.log(this.props);
     return(
     <>
     <Header {...this.props}></Header>
     <Hero data={landingPage.hero} />
-    <MostPicked data={landingPage.MostPicked} />
+    <MostPicked data={landingPage.mostPicked} />
 
       
 
--- src/utils/formatNumber.js
+++ src/utils/formatNumber.js
@@ -1,3 +1,4 @@
+// eslint-disable-next-line import/no-anonymous-default-export
 export default number => {
   const formatNumbering = new Intl.NumberFormat("id-ID");
   return formatNumbering.format(number);
