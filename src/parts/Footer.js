88ef6fea8fbd5291f17654e844ec7f45aa242463
nicogulo <nicogulobelas@gmail.com>
2021-04-13 20:10:22 +0700

    start Add Footer

---
+++ src/parts/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Button from 'elements/Button'
+export default function Footer() {
+  return (
+    <footer>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+          
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
