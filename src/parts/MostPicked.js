30f9b049aa8ec2a35228e7f84fc31a3c88594ed9
nicogulo <nicogulobelas@gmail.com>
2021-04-13 20:59:48 +0700

    footer part3

--- src/parts/MostPicked.js
+++ src/parts/MostPicked.js
@@ -19,7 +19,7 @@ export default function MostPicked(props) {
                       <img src={item.imageUrl} alt={item.name} className="img-cover"/>
                   </figure>
                   <div class="meta-wrapper">
-                     <Button type="link" className="streched-link d-block text-white" href={`/properties/${item._id}`}>
+                     <Button type="link" className="stretched-link d-block text-white" href={`/properties/${item._id}`}>
                       <h5>{item.name}</h5>
                      </Button>
                      <span>
