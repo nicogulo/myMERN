5690475c03e3d5b1a67c15479173fbd9064b7cc3
nicogulo <nicogulobelas@gmail.com>
2021-04-22 13:50:46 +0700

    start input date and use react hooks

---
+++ src/elements/Form/InputDate/index.js
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from "react";
+import propTypes from "prop-types";
+
+import { DateRange } from "react-date-range";
+
+import "./index.scss";
+import "react-date-range/dist/styles.css"; // main css file
+import "react-date-range/dist/theme/default.css"; // theme css file
+
+import formatDate from "utils/formatDate";
+import iconCalendar from "assets/images/icons/icon-calendar.svg";
