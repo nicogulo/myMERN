2e3dc2a868242208a07475a759d69b282270e9ec
nicogulo <nicogulobelas@gmail.com>
2021-04-25 23:40:20 +0700

    booking form p2 and its bug

--- src/parts/BookingForm.js
+++ src/parts/BookingForm.js
@@ -2,8 +2,7 @@ import React, { Component } from "react";
 
 import propTypes from "prop-types";
 import Button from "elements/Button";
-import { InpputNumber, InputDate } from "elements/Form";
-import Date from "elements/Form/InputDate";
+import { InputDate, InputNumber } from "elements/Form";
 
 export default class BookingForm extends Component {
   // superprops untuk membaca bagian kiri
@@ -23,39 +22,43 @@ export default class BookingForm extends Component {
     };
   }
 
-  // Function updateData menerima event target
-  updateData = (e) => {
-    this.setState({
-      ...this.state,
-      data: {
-        ...this.state.data,
-        [e.target.name]: e.target.value,
-      },
-    });
-  };
+  // eslint-disable-next-line no-undef
+  // updateData = (e) => {
+  //   this.setState({
+  //     ...this.state,
+  //     data: {
+  //       ...this.state.data,
+  //       [e.target.name]: e.target.value,
+  //     },
+  //   });
+  // };
 
   componentDidUpdate(prevProps, prevState) {
     const { data } = this.state;
-    // kondisi pertama(date): jika tidak sama makan akan menjalankan perintah
+    // kondisi pertama(date): jika Date tidak sama makan akan menjalankan perintah
     if (prevState.data.date !== data.date) {
       const startDate = new Date(data.date.startDate);
       const endDate = new Date(data.date.endDate);
       const countDuration = new Date(endDate - startDate).getDate();
       // mengganti state local diatas menjadi data baru
       this.setState({
-        date: {
+        data: {
           ...this.state.data,
           duration: countDuration,
         },
       });
     }
 
-    if (prevState.data.duration !== DeviceAcceleration.duration) {
+    // kondisi kedua(duration): jika duration tidak sama makan akan menjalankan perintah
+    if (prevState.data.duration !== data.duration) {
       const startDate = new Date(data.date.startDate);
+      // dapat kan nilai dari startDate dari local state lalu ditambah dengan duration baru
       const endDate = new Date(
-        startDate.setDate(startDate.setDate.getDate() + +data.duration - 1)
+        startDate.setDate(startDate.getDate() + +data.duration - 1)
       );
-      this.setDate({
+
+      // update state local
+      this.setState({
         ...this.state,
         data: {
           ...this.state.data,
@@ -68,11 +71,60 @@ export default class BookingForm extends Component {
     }
   }
   render() {
-    return <div></div>;
+    // menDistructor state dan props untuk menerima
+    const { data } = this.state;
+    // eslint-disable-next-line no-unused-vars
+    const { itemDetails, startBooking } = this.props;
+
+    return (
+      <div className="card bordered" style={{ padding: "60px 80px" }}>
+        <h1 className="mb-3">Start Booking</h1>
+        <h5 className="h2 text-teal mb-4">
+          ${itemDetails.price}{" "}
+          <span className="text-gray-500 font-weight-light">
+            per {itemDetails.unit}
+          </span>
+        </h5>
+        <label htmlFor="duration">How long you will stay</label>
+        <InputNumber
+          max={30}
+          suffix={" Night"}
+          isSuffixPlural
+          onChange={this.updateData}
+          name={"duration"}
+          value={data.duration}
+        />
+        <label htmlFor="date">Pick a date</label>
+        <InputDate onChange={this.updateData} name="date" value={data.date} />
+
+        <h6
+          className="text-gray-500 font-weight-light"
+          style={{ marginBottom: 40 }}
+        >
+          You will pay{" "}
+          <span className="text-gray-900">
+            ${itemDetails.price * data.duration} USD
+          </span>{" "}
+          per{" "}
+          <span className="text-gray-900">
+            {data.duration} {itemDetails.unit}
+          </span>
+        </h6>
+        <Button
+          className="btn"
+          hasShadow
+          isPrimary
+          isBlock
+          onClick={this.startBooking}
+        >
+          Continue to Book
+        </Button>
+      </div>
+    );
   }
 }
 
 BookingForm.propTypes = {
   itemDetails: propTypes.object,
-  startBookinig: propTypes.func,
+  startBooking: propTypes.func,
 };
