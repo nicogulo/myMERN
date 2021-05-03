7a98116c2506fb4e152939d09b17fcec151a7080
nicogulo <nicogulobelas@gmail.com>
2021-05-03 15:41:41 +0700

    disabled eslint

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -1,9 +1,10 @@
-export default date => {
+// eslint-disable-next-line import/no-anonymous-default-export
+export default (date) => {
   const d = new Date(date);
   const dtf = new Intl.DateTimeFormat("en", {
     year: "numeric",
     month: "short",
-    day: "2-digit"
+    day: "2-digit",
   });
   const [{ value: mo }, , { value: da }] = dtf.formatToParts(d);
 
