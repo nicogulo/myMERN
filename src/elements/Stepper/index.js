7251f0620b27d9d77ad8e8750c68fb3fffbcfae8
nicogulo <nicogulobelas@gmail.com>
2021-05-12 19:22:01 +0700

    component stepper

--- src/elements/Stepper/index.js
+++ src/elements/Stepper/index.js
@@ -4,11 +4,15 @@ import propTypes from "prop-types";
 export default function Stepper(props) {
   const { steps, initialStep } = props;
   const stepsKeys = Object.keys(steps);
+
+  // step lokal
   const [CurrentStep, setCurrentStep] = useState(
     stepsKeys.indexOf(initialStep) > -1 ? initialStep : stepsKeys[0]
   );
-  const totalStep = Object.keys(steps).lenght;
+  // hasil dari step 
+  const totalStep = stepsKeys.lenght;
   const indexStep = stepsKeys.indexOf(CurrentStep);
+  
   function prevStep() {
     if (+indexStep > 0) setCurrentStep(stepsKeys[indexStep - 1]);
   }
